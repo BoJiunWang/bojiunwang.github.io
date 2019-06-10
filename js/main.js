bb429fdccc5fd4e8858b33ceb2853b97f42408af
Ivan_Wang <bobo8347@gmail.com>
2019-06-10 21:40:00 +0800

    - Move naughty bug js code to main.js - Restyle code

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
-$(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-})
+$(function() {
+    $('[data-toggle="tooltip"]').tooltip();
+
+    var divSize = ((Math.random()*100) + 50).toFixed();
+    var posX = (Math.random() * ($(document).width() - divSize)).toFixed();
+    var posY = (Math.random() * ($(document).height() - divSize)).toFixed();
+    var timer = window.setInterval(function(){
+                    $('#bug').css({
+                        'color':'#'+ Math.round(0xffffff * Math.random()).toString(16)
+                    });
+                }, 1000);
+
+    $('#naughty-bug').css({
+        'position':'absolute',
+        'left':posX+'px',
+        'top':posY+'px',
+    });
+    $('#naughty-bug').fadeIn(1000).delay(8000).fadeOut(1000, function(){
+        clearInterval(timer);
+        $('#bug').tooltip('hide');
+        $(this).remove();
+    }); 
+})();

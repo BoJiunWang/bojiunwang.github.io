a1dc4a146dc9637354c89b8d049071336d62cea1
Ivan Wang <bobo8347@gmail.com>
2020-04-18 01:44:21 +0800

    - Modified css animation

--- js/main.js
+++ js/main.js
@@ -1,31 +1,31 @@
-$(function() {
-    $('[data-toggle="tooltip"]').each(function(){
+$(function () {
+    $('[data-toggle="tooltip"]').each(function () {
         $(this).tooltip();
     });
 
-    var divSize = ((Math.random()*100) + 50).toFixed();
+    var divSize = ((Math.random() * 100) + 50).toFixed();
     var posX = (Math.random() * ($(document).width() - divSize)).toFixed();
     var posY = (Math.random() * ($(document).height() - divSize)).toFixed();
 
-    var timer = window.setInterval(function(){
-                    $('#bug').css({
-                        'color':'#'+ Math.round(0xffffff * Math.random()).toString(16)
-                    });
-                }, 1000);
+    var timer = window.setInterval(function () {
+        $('#bug').css({
+            'color': '#' + Math.round(0xffffff * Math.random()).toString(16)
+        });
+    }, 1000);
 
     $('#naughty-bug').css({
         'position': 'absolute',
-        'left': posX+'px',
-        'top': posY+'px'
+        'left': posX + 'px',
+        'top': posY + 'px'
     });
-    
-    $('#naughty-bug').fadeIn(1000).delay(8000).fadeOut(1000, function(){
+
+    $('#naughty-bug').fadeIn(1000).delay(8000).fadeOut(1000, function () {
         clearInterval(timer);
         $('#bug').tooltip('hide');
         $(this).remove();
-    }); 
+    });
 
-    if(window.matchMedia("(prefers-color-scheme: dark)").matches)
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches)
         $("body").addClass("dark-mode");
 });
 
@@ -34,10 +34,31 @@ function toggleDarkLight() {
 }
 
 function cancelOpacityAndRunScale(element) {
+    // Find Old element and remove it
+    var logoStr = document.getElementById("logoStr");
+    logoStr && element.removeChild(logoStr);
+
+    // Create New Logo String
+    var logoStrDiv = document.createElement('div');
+    logoStrDiv.setAttribute("id", "logoStr");
+    logoStrDiv.innerHTML = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style="color:#2F9BD6;font-weight:700">GAME</span><span style="color:#EF8218;font-weight:700">SOFa</span>';
+    element.appendChild(logoStrDiv);
+
+    // Update Logo Opacity
     element.style.opacity = 1;
+    // Add runScale css
     $(element).addClass('runScale');
+
+    // Animation End Callback
     element.addEventListener('animationend', function () {
+        // Reset Opacity
         element.style.opacity = 0.25;
+        // Remove runScale css
         $(element).removeClass('runScale')
+        // Remove New Logo String
+        var logoStr = document.getElementById("logoStr");
+        logoStr && element.removeChild(logoStr);
+        // Hide tooltip
+        $('#gamesofaLogo').tooltip('hide');
     });
 }
